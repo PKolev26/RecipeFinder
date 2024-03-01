a5d802e9bc23c57cb09e8d7900b1a11bd9f1fcf4
Kolev <kolevaz@gmail.com>
2024-03-01 17:47:05 +0200

    Added Category and Ingredient Constants

---
+++ RecipeFinder.Infrastructure/Constants/CategoryDataConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeFinder.Infrastructure.Constants
+{
+    public class CategoryDataConstants
+    {
+        // Category Name Data Constants
+        public const int CategoryNameMaxLength = 25;
+        public const int CategoryNameMinLength = 5;
+    }
+}
---
+++ RecipeFinder.Infrastructure/Constants/IngredientDataConstants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeFinder.Infrastructure.Constants
+{
+    public class IngredientDataConstants
+    {
+        // Ingredient Name Data Constants
+        public const int IngredientNameMaxLength = 50;
+        public const int IngredientNameMinLength = 3;
+
+        // Ingredient Quantity Data Constants
+        public const double IngredientQuantityMaxLength = 0.01;
+        public const double IngredientQuantityMinLength = 999.99;
+
+        // Ingredient Unit Data Constants
+        public const int IngredientUnitMaxLength = 10;
+        public const int IngredientUnitMinLength = 1;
+
+    }
+}

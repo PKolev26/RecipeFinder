351fbe028bc6a67cbc803ceaf352dbbb93cc79eb
Kolev <kolevaz@gmail.com>
2024-03-01 11:39:01 +0200

    Added Infrastrucuture Folders and minimal changes

---
+++ RecipeFinder.Infrastructure/Data/RecipeFinderDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecipeFinder.Data
+{
+    public class RecipeFinderDbContext : IdentityDbContext
+    {
+        public RecipeFinderDbContext(DbContextOptions<RecipeFinderDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}

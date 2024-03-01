7a5c68c592bc8b7795d71a52cf00993a219c0652
Kolev <kolevaz@gmail.com>
2024-03-01 11:39:33 +0200

    Adding a very cool favicon

--- RecipeFinder/Program.cs
+++ RecipeFinder/Program.cs
@@ -12,12 +12,12 @@ namespace RecipeFinder
 
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            builder.Services.AddDbContext<RecipeFinderDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<RecipeFinderDbContext>();
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();

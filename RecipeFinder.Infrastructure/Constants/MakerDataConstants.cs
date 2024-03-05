acce930d0b442e2864800b186023000c62b4ab00
Kolev <kolevaz@gmail.com>
2024-03-05 20:45:28 +0200

    Adding Maker Constants

---
+++ RecipeFinder.Infrastructure/Constants/MakerDataConstants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeFinder.Infrastructure.Constants
+{
+    public static class MakerDataConstants
+    {
+
+        // Maker FirstName Data Constants
+        public const int MakerFirstNameMaxLength = 30;
+        public const int MakerFirstNameMinLength = 3;
+
+        // Maker LastName Data Constants
+        public const int MakerLastNameMaxLength = 30;
+        public const int MakerLastNameMinLength = 3;
+
+        // Maker SkillLevel Data Constants
+        public const double MakerSkillLevelMin = 0.01;
+        public const double MakerSkillLevelMax = 10.00;
+    }
+}

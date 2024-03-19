c4337f8b53b41991f12737300b5ebb9aaa821391
pkolev26 <kolevaz@gmail.com>
2024-03-19 08:46:08 +0200

    IRepository and Repository added

---
+++ RecipeFinder.Infrastructure/Common/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeFinder.Infrastructure.Common
+{
+    public interface IRepository
+    {
+
+    }
+}
---
+++ RecipeFinder.Infrastructure/Common/Repository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeFinder.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeFinder.Infrastructure.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly DbContext dbContext;
+
+        public Repository(RecipeFinderDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}

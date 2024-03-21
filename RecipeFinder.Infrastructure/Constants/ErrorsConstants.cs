02f65ed6eb05258e2179b4e888f8a8fe683cf15e
pkolev26 <kolevaz@gmail.com>
2024-03-21 08:49:25 +0200

    Added error constants

---
+++ RecipeFinder.Infrastructure/Constants/ErrorsConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeFinder.Infrastructure.Constants
+{
+    public static class ErrorsConstants
+    {
+        public const string RequiredErrorMessage = "This {0} field is required.";
+        public const string LengthErrorMessage = "This {0} field needs to be between {2} and {1} symbols.";
+    }
+}

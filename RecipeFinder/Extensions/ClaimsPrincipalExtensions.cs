8cbe5eb5d2d4823752a5761c707210681ba0c4f5
Kolev <kolevaz@gmail.com>
2024-03-23 18:23:50 +0200

    Added ClaimsPrincipalExtension

---
+++ RecipeFinder/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace RecipeFinder.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
